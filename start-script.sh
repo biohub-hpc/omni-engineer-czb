e4b53ede73f2df6f865c17ef1c6236667e95b1ac
Mark A Potts <mark.potts@noaa.gov>
2025-07-09 05:01:56 -0700

    updated start-script

--- start-script.sh
+++ start-script.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 killall ollama >& /dev/null
-ollama start >& /dev/null &
-alias omni-engineer="python $PWD/main.py"
+module list 
+# need to use the full path or the conda version will run very slowly
+/hpc/apps/ollama/0.6.8/bin/ollama start >& /dev/null &
+pushd . > '/dev/null';
+SCRIPT_PATH="${BASH_SOURCE[0]:-$0}";
+
+while [ -h "$SCRIPT_PATH" ];
+do
+    cd "$( dirname -- "$SCRIPT_PATH"; )";
+    SCRIPT_PATH="$( readlink -f -- "$SCRIPT_PATH"; )";
+done
+
+cd "$( dirname -- "$SCRIPT_PATH"; )" > '/dev/null';
+SCRIPT_PATH="$( pwd; )";
+popd  > '/dev/null';
+alias omni-engineer="python $SCRIPT_PATH/main.py"
 
